0d69f8646e9d03f343a0c008555fb2cc5700b1e4
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-02-02 15:00:20 -0500

    Endpoint Login inicial

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
+app = FastAPI()
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+@app.post("/login")
+async def login(login_request : LoginRequest):
+    if (login_request.username == "PW" and login_request.password == "123"):
+        return {
+            "msg": "Acceso concedido"
+        }
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail="Error en login, credenciales incorrectas")
+    
