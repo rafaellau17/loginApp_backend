39c898b859a59349803335696b199d28d8a43f63
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-02-16 15:08:33 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import uuid
-from sqlalchemy import UUID, Column, DateTime, String
+from sqlalchemy import UUID, Column, DateTime, String, ForeignKey
 from .database import Base
+from sqlalchemy.orm import relationship
 
 class Usuario(Base):
     __tablename__ = "usuario"
@@ -12,6 +13,23 @@ class Usuario(Base):
     )
     username = Column(String, unique=True)
     password = Column(String, unique=True)
+    perfil = relationship("Perfil", back_populates="usuario", uselist=False) # Relacion 1 a 1
+
+class Perfil(Base):
+    __tablename__ = "perfil"
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=lambda: str(uuid.uuid4()),
+        index=True
+    )
+    nombre = Column(String)
+    pais = Column(String)
+    usuario_id = Column(
+        String(36),
+        ForeignKey("usuario.id", unique=True)
+    )
+    usuario = relationship("Usuario", back_populates="perfil", uselist=False)
 
 class Acceso(Base):
     __tablename__ = "acceso"
